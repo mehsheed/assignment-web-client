40c674352607758b39274238adfee703c43e5610
Mehsheed Ahmed Syed Abdul <mehsheed59@gmail.com>
2023-10-12 19:50:47 -0600

    feat : Implement GET

--- httpclient.py
+++ httpclient.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 # coding: utf-8
 # Copyright 2016 Abram Hindle, https://github.com/tywtyw2002, and https://github.com/treedust
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -21,37 +21,57 @@
 import sys
 import socket
 import re
+
 # you may use urllib to encode data appropriately
-import urllib.parse
+from urllib.parse import urlparse
+
 
 def help():
     print("httpclient.py [GET/POST] [URL]\n")
+
 
 class HTTPResponse(object):
     def __init__(self, code=200, body=""):
         self.code = code
         self.body = body
 
+
 class HTTPClient(object):
-    #def get_host_port(self,url):
+    # def get_host_port(self,url):
 
     def connect(self, host, port):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.connect((host, port))
         return None
 
+    def parse_url(self, url):
+        parsed_obj = urlparse(url)
+        port = parsed_obj.port
+        path = parsed_obj.path
+
+        if not port:
+            port = 80
+        if not path:
+            path = "/"
+
+        return parsed_obj.hostname, port, path
+
     def get_code(self, data):
+        for line in data.splitlines():
+            if line.startswith("HTTP/"):
+                return int(line.split()[1])
+
         return None
 
-    def get_headers(self,data):
+    def get_headers(self, data):
         return None
 
     def get_body(self, data):
-        return None
-    
+        return data.split("\r\n\r\n")[1]
+
     def sendall(self, data):
-        self.socket.sendall(data.encode('utf-8'))
-        
+        self.socket.sendall(data.encode("utf-8"))
+
     def close(self):
         self.socket.close()
 
@@ -61,15 +81,23 @@ class HTTPClient(object):
         done = False
         while not done:
             part = sock.recv(1024)
-            if (part):
+            if part:
                 buffer.extend(part)
             else:
                 done = not part
-        return buffer.decode('utf-8')
+        return buffer.decode("utf-8")
 
     def GET(self, url, args=None):
-        code = 500
-        body = ""
+        hostname, port, path = self.parse_url(url)
+        self.connect(hostname, port)
+        request = "GET {} HTTP/1.1\r\nHost: {}\r\nAccept: */*\r\nConnection: Closed\r\n\r\n".format(
+            path, hostname
+        )
+        self.sendall(request)
+        response = self.recvall(self.socket)
+        code = self.get_code(response)
+        body = self.get_body(response)
+        self.close()
         return HTTPResponse(code, body)
 
     def POST(self, url, args=None):
@@ -78,18 +106,19 @@ class HTTPClient(object):
         return HTTPResponse(code, body)
 
     def command(self, url, command="GET", args=None):
-        if (command == "POST"):
-            return self.POST( url, args )
+        if command == "POST":
+            return self.POST(url, args)
         else:
-            return self.GET( url, args )
-    
+            return self.GET(url, args)
+
+
 if __name__ == "__main__":
     client = HTTPClient()
     command = "GET"
-    if (len(sys.argv) <= 1):
+    if len(sys.argv) <= 1:
         help()
         sys.exit(1)
-    elif (len(sys.argv) == 3):
-        print(client.command( sys.argv[2], sys.argv[1] ))
+    elif len(sys.argv) == 3:
+        print(client.command(sys.argv[2], sys.argv[1]))
     else:
-        print(client.command( sys.argv[1] ))
+        print(client.command(sys.argv[1]))
